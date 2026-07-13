35f9e6ad96ef90d3d8eea252ee26d3f9341e0e7a
PujanKadecha <pujannk@gmail.com>
2026-07-13 17:49:57 +0530

    feat: add Redis integration for improved caching and data management

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,23 @@ const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      `Server running and listening on IPv4 at http://localhost:${PORT}`,
-    );
-  });
-});
+const redisClient = require("./config/redis");
+
+async function startServer() {
+  try {
+    await connectDB();
+
+    await redisClient.connect();
+
+    app.listen(PORT, () => {
+      console.log(
+        `Server running and listening on IPv4 at http://localhost:${PORT}`,
+      );
+    });
+  } catch (err) {
+    console.error("Server startup failed:", err);
+    process.exit(1);
+  }
+}
+
+startServer();
